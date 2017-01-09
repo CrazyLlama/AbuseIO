8c75d40a7d61d204307a0de3c2a66f25bc365ce5
Brett <CrazyLlama@users.noreply.github.com>
2017-01-09 14:01:25 +0000

    Adding ;

--- Parser_problems/cleanse_the_mail_bash_attempt.php
+++ Parser_problems/cleanse_the_mail_bash_attempt.php
@@ -4,4 +4,4 @@
 // help
 // Maybe this works?
 
-$cleanse = shell_exec("sed 's/Received\:\*from\*\[[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\]\*by\*spamcop\.net\*with\*HTTP\;\*Thu\,\*[0-9][0-9]\*[A-Z][a-z][a-z]\*[0-9][0-9][0-9]\n\*[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]\*GMT//g' /opt/abuseio/email.eml")
+$cleanse = shell_exec("sed 's/Received\:\*from\*\[[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\]\*by\*spamcop\.net\*with\*HTTP\;\*Thu\,\*[0-9][0-9]\*[A-Z][a-z][a-z]\*[0-9][0-9][0-9]\n\*[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]\*GMT//g' /opt/abuseio/email.eml");
