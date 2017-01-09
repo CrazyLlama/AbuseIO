06e55c53877f37c3df44c1f4e8244c0c8cb8449f
Brett <CrazyLlama@users.noreply.github.com>
2017-01-09 13:59:12 +0000

    Escaping s' in sed
    I hope it works now

--- Parser_problems/cleanse_the_mail_bash_attempt.php
+++ Parser_problems/cleanse_the_mail_bash_attempt.php
@@ -2,6 +2,6 @@
 
 // s/Received\:*from*\[[0-9][0-9][0-9].[0-9][0-9][0-9].[0-9][0-9][0-9].[0-9][0-9][0-9]\]*by*spamcop.net*with*HTTP\;*Thu\,*[0-9][0-9]*[A-Z][a-z][a-z]*[0-9][0-9][0-9]\n*[0-9][0-9]:[0-9][0-9]:[0-9][0-9]*GMT//g
 // help
+// Maybe this works?
 
-$cleanse = shell_exec("sed 's/Received\:*from*\[[0-9][0-9][0-9].[0-9][0-9][0-9].[0-9][0-9][0-9].[0-9][0-9][0-9]\]*by*spamcop.net*with*HTTP\;*Thu\,*[0-9][0-9]*[A-Z][a-z][a-z]*[0-9][0-9][0-9]\n*[0-9][0-9]:[0-9][0-9]:[0-9][0-9]*GMT//g' /opt/abuseio/email.eml")
-?php>
+$cleanse = shell_exec("sed 's/Received\:\*from\*\[[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\]\*by\*spamcop\.net\*with\*HTTP\;\*Thu\,\*[0-9][0-9]\*[A-Z][a-z][a-z]\*[0-9][0-9][0-9]\n\*[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]\*GMT//g' /opt/abuseio/email.eml")
